7bf153228d9d48d34d33a586c0b56ec14b299079
Kyle Terramoccia <kyleterramoccia@kyles-mbp.myfiosgateway.com>
2021-03-08 19:15:35 -0500

    Adding basic variables and anchors

--- Leaflet-Step-1/assets/js/app.js
+++ Leaflet-Step-1/assets/js/app.js
@@ -1,1 +1,15 @@
-// @TODO: YOUR CODE HERE!
+// Anchor map w/ background image
+var myMap = L.map("map", {
+    // center of the United States
+    center: [39.8283, -98.5795],
+    zoom: 0
+  });
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: "API KEY"
+  }).addTo(myMap);
+
+  // Set URL variable as All Earthquakes from the last month
+  var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
