68026f2087e9b367d85e885217cf65cedc5739cf
Kyle Terramoccia <kyleterramoccia@kyles-mbp.myfiosgateway.com>
2021-03-08 21:07:48 -0500

    Updating code

--- Leaflet-Step-1/assets/js/app.js
+++ Leaflet-Step-1/assets/js/app.js
@@ -8,13 +8,13 @@ var myMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: "API KEY"
+    accessToken: "pk.eyJ1Ijoia3lsZTEyNXQiLCJhIjoiY2tsc25kZGVwMGJxNjJvbDhtcG9jaW1hbSJ9.gR7NsrGBYIZugXwP1BrpCA"
   }).addTo(myMap);
 
   // Set URL variable as All Earthquakes from the last month
   var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
-  // Legend colors
+  // Legend/magnitude colors
   // green: #ff3333
   // yellow green: #ff8333
   // yellow: #ffbe33
@@ -33,4 +33,24 @@ var myMap = L.map("map", {
         stroke: true,
         weight: 0.5
       };
+    }
+    function magColor(magnitude) {
+        switch (true) {   
+            case magnitude > 1:
+                return "#ff8333";
+
+            case magnitude > 2:
+                return "#ffbe33";
+
+            case magnitude > 3:
+                return "#fcff33";
+
+            case magnitude > 4:
+                return "#c4ff33";
+
+            case magnitude > 5:
+                return "#2ecc71";
+            default:
+                return "#ff3333";
+        };
     }
